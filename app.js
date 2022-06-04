0acad6614c0ba0d73dbee8e5d03227b0a5c291cb
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-04 11:56:51 +0300

    Add hour handle

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const app = document.getElementById('app');
 const second = document.querySelector('.second');
 const minute = document.querySelector('.minute');
+const hour = document.querySelector('.hour');
 
-const rotateClock = (sec, min) => {
+const rotateClock = (sec, min, hr) => {
   const d = new Date();
   let secDeg = (6 * d.getSeconds()) - 90;
   let minDeg = (6 * d.getMinutes()) - 90;
-  sec.style.transform = `rotate(${secDeg}deg) translateX(60px)`;
+  let hourDeg = (30 * d.getHours()) - 90;
+  sec.style.transform = `rotate(${secDeg}deg) translateX(50px)`;
   min.style.transform = `rotate(${minDeg}deg) translateX(45px)`;
+  hr.style.transform = `rotate(${hourDeg}deg) translateX(35px)`;
 };
 
-rotateClock(second, minute);
+rotateClock(second, minute, hour);
 second.style.display = "block";
 minute.style.display = "block";
+hour.style.display = "block";
 
 setInterval(() => {
-  rotateClock(second, minute);
+  rotateClock(second, minute, hour);
 }, 1000);
