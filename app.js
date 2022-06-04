5f1d235fd6c3f1b1d7eec0d721c33a4643f2ef51
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-06-04 17:41:25 +0300

    Add the minutes diviation to the hours handle

--- app.js
+++ app.js
@@ -7,10 +7,11 @@ const rotateClock = (sec, min, hr) => {
   const d = new Date();
   let secDeg = (6 * d.getSeconds()) - 90;
   let minDeg = (6 * d.getMinutes()) - 90;
-  let hourDeg = ((30 * d.getHours()) - 90) + ((5 * minDeg)/360);
+  let hourDeg = ((30 * d.getHours()) - 90) + ((30 * (minDeg + 90))/360);
   sec.style.transform = `rotate(${secDeg}deg) translateX(45px)`;
   min.style.transform = `rotate(${minDeg}deg) translateX(40px)`;
   hr.style.transform = `rotate(${hourDeg}deg) translateX(30px)`;
+  console.log(secDeg, minDeg, hourDeg);
 };
 
 rotateClock(second, minute, hour);
