3bb5008d1dd6406a8a173dd5950c3f9b3aaa07da
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-12-25 16:58:45 +0300

    refactor: change the folder structure of the project

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,18 @@ const hour = document.querySelector('.hour');
 
 const rotateClock = (sec, min, hr) => {
   const d = new Date();
-  let secDeg = (6 * d.getSeconds()) - 90;
-  let minDeg = (6 * d.getMinutes()) - 90;
-  let hourDeg = ((30 * d.getHours()) - 90) + ((30 * (minDeg + 90))/360);
+  let secDeg = 6 * d.getSeconds() - 90;
+  let minDeg = 6 * d.getMinutes() - 90;
+  let hourDeg = 30 * d.getHours() - 90 + (30 * (minDeg + 90)) / 360;
   sec.style.transform = `rotate(${secDeg}deg) translateX(45px)`;
   min.style.transform = `rotate(${minDeg}deg) translateX(40px)`;
   hr.style.transform = `rotate(${hourDeg}deg) translateX(30px)`;
 };
 
 rotateClock(second, minute, hour);
-second.style.display = "block";
-minute.style.display = "block";
-hour.style.display = "block";
+second.style.display = 'block';
+minute.style.display = 'block';
+hour.style.display = 'block';
 
 setInterval(() => {
   rotateClock(second, minute, hour);
