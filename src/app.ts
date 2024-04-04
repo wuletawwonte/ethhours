e91ad9e084a6d2bf5df1332edf39d269646e4e9e
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-04-04 20:47:00 +0300

    ignore prettier autohide

--- src/app.ts
+++ src/app.ts
@@ -44,8 +44,9 @@ const myTime = new Proxy(timeObj, {
 setInterval(() => {
   myTime.second = (new Date().getSeconds() / 60) * 360 - 90;
   myTime.minute = (new Date().getMinutes() / 60) * 360 - 90;
+  // prettier-ignore
   myTime.hour =
-    (new Date().getHours() / 12) * 360 -
+    ((new Date().getHours() / 12) * 360) -
     90 +
-    (new Date().getMinutes() / 60) * 30;
+    ((new Date().getMinutes() / 60) * 30);
 }, 1000);
