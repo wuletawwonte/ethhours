b88c22fc2d719012cba2cf26a970e5f1152863b9
Wuletaw Wonte <wuletawwonte@gmail.com>
2024-04-04 12:03:12 +0300

    Refactor using proxy (#7)
    * create ref wrapper function
    
    * create a render function with rotation styles
    
    * listen for changes in the props of the time object
    
    * Get rid of commented code
    
    * fix degree calculation for time bars
    
    * remove unused argument in proxy setter method

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,48 @@
-const app = document.getElementById('app');
-const second = document.querySelector<HTMLElement>('.second');
-const minute = document.querySelector<HTMLElement>('.minute');
-const hour = document.querySelector<HTMLElement>('.hour');
+const render = (timeDeg: TimeType) => {
+  const app = document.getElementById('app');
+  if (!app) return;
+  app.innerHTML = `<div class="app-content">
+  <hr class="second" style="transform: rotate(${timeDeg.second}deg) translateX(45px)" />
+  <hr class="minute" style="transform: rotate(${timeDeg.minute}deg) translateX(40px)" />
+  <hr class="hour" style="transform: rotate(${timeDeg.hour}deg) translateX(30px)" />
+  <hr class="dot" />
+  <div class="hour-characters">
+    <h3>፲፪</h3>
+    <div class="middle-hour-characters">
+      <h3>፱</h3>
+      <h3>፫</h3>
+    </div>
+    <h3>፮</h3>
+  </div>
+</div>
+`;
+};
 
-if (second === null || minute === null || hour === null) {
-  throw new Error('Element not found');
+interface TimeType {
+  second: number;
+  minute: number;
+  hour: number;
 }
 
-const updateClock = () => {
-  const d = new Date();
-  const secDeg = 6 * d.getSeconds() - 90;
-  const minDeg = 6 * d.getMinutes() - 90;
-  const hourDeg = 30 * d.getHours() - 90 + (30 * (minDeg + 90)) / 360;
-  return { secDeg, minDeg, hourDeg };
+const timeObj: TimeType = {
+  second: 0,
+  minute: 0,
+  hour: 0,
 };
 
-const rotateClock = (sec: HTMLElement, min: HTMLElement, hr: HTMLElement) => {
-  const { secDeg, minDeg, hourDeg } = updateClock();
-  sec.style.transform = `rotate(${secDeg}deg) translateX(45px)`;
-  min.style.transform = `rotate(${minDeg}deg) translateX(40px)`;
-  hr.style.transform = `rotate(${hourDeg}deg) translateX(30px)`;
-};
-
-rotateClock(second, minute, hour);
-second.style.display = 'block';
-minute.style.display = 'block';
-hour.style.display = 'block';
+const myTime = new Proxy(timeObj, {
+  get(target: TimeType, key: 'second' | 'minute' | 'hour') {
+    return target[key];
+  },
+  set(target: TimeType, key: 'second' | 'minute' | 'hour', value) {
+    target[key] = value;
+    render(target);
+    return true;
+  },
+});
 
 setInterval(() => {
-  rotateClock(second, minute, hour);
+  myTime.second = (new Date().getSeconds() / 60) * 360 - 90;
+  myTime.minute = (new Date().getMinutes() / 60) * 360 - 90;
+  myTime.hour = (new Date().getHours() / 12) * 360 - 90;
 }, 1000);
